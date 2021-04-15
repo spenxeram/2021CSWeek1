e44f332d645d3dcd8c061534c05ca12ac7d909e8
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-15 11:02:17 +0700

    update2

--- Final2021/index.php
+++ Final2021/index.php
@@ -36,8 +36,21 @@ $navLinks = ["Home", "Womens", "Mens", "Kids"];
       </div>
     </div>
   </nav>
+  <div class="jumbotron jumbotron-fluid text-center">
 
+  </div>
+<h1 class="text-center font-weight-light">Big Sale!</h1>
+<hr>
 
+  <main>
+
+  </main>
+<hr>
+
+<footer>
+
+<p class="text-center"> Copyright ITEC 2021</p>
+</footer>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
