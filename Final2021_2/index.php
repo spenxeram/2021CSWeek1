865165a08b2a76a165c72ecb7227c4e2ef4a92f7
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-15 15:21:56 +0700

    Update1

--- Final2021_2/index.php
+++ Final2021_2/index.php
@@ -1,7 +1,9 @@
+<?php
+$navlinks = ["Home", "Clothes", "Watches", "Other"];
+ ?>
 <!doctype html>
 <html lang="en-us">
 <head>
-  
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,7 +18,41 @@
   <!-- This is the CS201 Web Dev 1 final. Your task is to make a responsive e-commerce site using
   BS4 as your CSS front end framework and load products from the FakeStore API (linked below) -->
   <!-- FakeStoreAPI end point: https://fakestoreapi.com/products -->
-  <h1>Anyeong World</h1>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ITEC CS 201 Final Review</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+       <?php
+        foreach ($navlinks as $link) {
+          echo '<a class="nav-link" href="#">' . $link . '</a>';
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron jumbotron-fluid">
+
+</div>
+<main>
+  <div class="container">
+    <hr>
+    <h2>Big Sale Today!</h2>
+    <div class="row products">
+
+    </div>
+    <hr>
+  </div>
+
+</main>
+
+<footer>
+  <p>Copyright 2021 ITEC Shopping.com</p>
+</footer>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
