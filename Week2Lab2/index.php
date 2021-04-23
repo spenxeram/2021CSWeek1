350514ac9da626bb61944e145f5dec2e6bfbbd3a
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-23 13:21:58 +0700

    Task1

--- Week2Lab2/index.php
+++ Week2Lab2/index.php
@@ -212,23 +212,7 @@
       </div>
     </div> <!-- end container -->
   </div> <!-- end dark bg -->
-  <div class="container">
-    <div class="row">
-      <div class="economy col-2">
-        <img src="images/economy.png" alt="">
-        <div class="right">
-          <h1>ECONOMIC MODEL</h1>
-          <p>The unique two-token system (VET+VTHO) significantly helps separate the cost of using blockchain from market speculation. Due to the correlation with the blockchain resource utilization, the cost is more predictable with the monitoring of the VTHO supply and demand. In addition, Foundation's governance mechanism further stabilizes the cost.</p><ul>
-            <li>Something 1</li>
-            <li>Something 2</li>
-            <li>Something 3</li>
 
-          </ul>
-        </div>
-      </div>
-    </div>
-
-  </div>
   <footer>
     <div class="container">
       <div class="row">
