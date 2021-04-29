94a8b69622564087d5c40249a886c411e91cbeaf
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-29 10:16:26 +0700

    Basic form check final

--- Week3/Week3Lect1/index.php
+++ Week3/Week3Lect1/index.php
@@ -1,14 +1,12 @@
 <?php
+// set error var to catch any errors, output warning alert if true/success if false
 $error = false;
 if(isset($_POST['submit'])) {
   // sanitize user inputs
-  var_dump($_POST);
   $username = htmlspecialchars($_POST['username']);
-  echo $username;
   $email = htmlspecialchars($_POST['email']);
   $password1 = htmlspecialchars($_POST['password1']);
   $password2 = htmlspecialchars($_POST['password2']);
-
 
   //validate user inputs
     // check username
@@ -26,13 +24,15 @@ if(isset($_POST['submit'])) {
     $password1_warning = "Password must be more than 5 characters!";
     $error = true;
   }
-
+    //check pw2
   if($password1 != $password2) {
     $password2_warning = "Passwords don't match, try again!";
     $error = true;
   }
 
-
+  if($error != true) {
+  //  header("Location: success.php");
+  }
 }
  ?>
 
@@ -62,16 +62,20 @@ if(isset($_POST['submit'])) {
       <p class="lead">Using filter_var() function and flags.</p>
     </div>
     <div class="container">
-      <?php var_dump($error); ?>
-      <?php if(isset($error)) if ($error): ?>
+      <?php if ($error): ?>
         <div class="alert alert-danger" role="alert">
-            Your form has some problems, please check for errors!
+              Your form has some problems, please check for errors!
+        </div>
+      <?php elseif($error == false && isset($_POST['submit'])): ?>
+        <div class='alert alert-success' role='alert'>
+            Your form has successfully been submitted, <?php echo $username; ?>
         </div>
       <?php endif; ?>
       <div class="row">
 
+
           <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <div class="mb-3">
+            <div class="mb-2 form-group">
               <label for="name" class="form-label">User Name</label>
               <input type="text" class="form-control" id="username" name="username" value="<?php if (isset($username)) {
                 echo $username;
@@ -80,7 +84,7 @@ if(isset($_POST['submit'])) {
                 echo $username_warning;
               } ?></div>
             </div>
-            <div class="mb-3">
+            <div class="mb-2 form-group form-group">
               <label for="email" class="form-label">Email address</label>
               <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="<?php if (isset($email)) {
                 echo $email;
@@ -89,28 +93,42 @@ if(isset($_POST['submit'])) {
                 echo $email_warning;
               } ?></div>
             </div>
-            <div class="mb-3">
+            <div class="mb-2 form-group">
               <label for="password1" class="form-label">Password</label>
               <input type="password" class="form-control" name="password1" value="">
-              <div class="warning"><?php if($password1_warning) {
+              <div class="warning"><?php if(isset($password1_warning)) {
                 echo $password1_warning;
               } ?></div>
             </div>
-            <div class="mb-3">
+            <div class="mb-2 form-group">
               <label for="password2" class="form-label">Confirm Password</label>
               <input type="password" class="form-control" name="password2" value="">
-              <div class="warning"><?php if($password2_warning) {
+              <div class="warning"><?php if(isset($password2_warning)) {
                 echo $password2_warning;
               } ?></div>
             </div>
-            <div class="mb-3 form-check">
+            <div class="mb-2 form-group form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1">
               <label class="form-check-label" for="exampleCheck1">Subscribe</label>
 
             </div>
             <button type="submit" class="btn btn-primary" name="submit">Submit</button>
         </form>
+
+        <?php
+        echo filter_var("<austinma@hawaii.edu>", FILTER_SANITIZE_EMAIL);
+        echo "<br>";
+        echo htmlspecialchars("<austinma@hawaii.edu>");
+        echo "<br>";
+        echo filter_var("<austinma@hawaii.edu>", FILTER_SANITIZE_STRING);
+        echo "<br>";
+        echo filter_var("<austinma@hawaii.edu>", FILTER_SANITIZE_ENCODED);
+
+         ?>
       </div>
+      <?php
+  var_dump($_POST);
+       ?>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
