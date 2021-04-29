5089e402bfbf47aca5842e487382df0af3aba195
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-29 13:31:43 +0700

    Update index.php

--- Week3/Week3Lect2/index.php
+++ Week3/Week3Lect2/index.php
@@ -5,13 +5,41 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <style media="screen">
+    .warning {
+      color: red;
+      font-style: italic;
+    }
+    </style>
   </head>
   <body>
     <!-- fixed-top | sticky-top | fixed-bottom -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#">ITEC Week 3.2 Filtering Input</a>
+        </div>
     </nav>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <div class="row">
+          <h1 class="display-3">Filtering User Input In PHP</h1>
+          <p class="lead">Using the filter_var() function and constants</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <form class="" action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+          <div class="mb-2">
+             <label for="username" class="form-label">User Name</label>
+             <input type="text" class="form-control" name="username">
+             <div class="warning">User name must be between 5 - 20 characters!</div>
+           </div>
+        </form>
+      </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
