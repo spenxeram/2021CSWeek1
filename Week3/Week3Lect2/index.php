761503d73d0ed809ccae5fee4c410363a8c671c4
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-04-29 13:37:12 +0700

    Update index.php

--- Week3/Week3Lect2/index.php
+++ Week3/Week3Lect2/index.php
@@ -37,9 +37,29 @@
              <input type="text" class="form-control" name="username">
              <div class="warning">User name must be between 5 - 20 characters!</div>
            </div>
+           <div class="mb-2">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" name="email">
+              <div class="warning">User name must be between 5 - 20 characters!</div>
+            </div>
+            <div class="mb-2">
+               <label for="password1" class="form-label">Password</label>
+               <input type="password" class="form-control" name="password1">
+               <div class="warning">User name must be between 5 - 20 characters!</div>
+             </div>
+             <div class="mb-2">
+                <label for="password2" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" name="password2">
+                <div class="warning">User name must be between 5 - 20 characters!</div>
+              </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block mb-5" name="submit">Submit Form</button>
         </form>
       </div>
     </div>
+
+    <footer style="background:black; color: white; text-align:center; padding: 30px;">
+      <h2>Itecky Design <?php echo date("Y"); ?></h2>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
