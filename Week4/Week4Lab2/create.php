f618df883c0d2f0f03aa95579d3b5ffbdc7677f2
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-07 13:46:01 +0700

    redirect

--- Week4/Week4Lab2/create.php
+++ Week4/Week4Lab2/create.php
@@ -2,6 +2,27 @@
 include 'db.php';
 include 'includes/header.php';
 
+if(isset($_POST['submit'])) {
+  $title = $_POST['title'];
+  $body = $_POST['body'];
+  $date = date("Y-m-d H:i:s");
+  // #1 prepare the statement sql with placeholders
+  $sql = "INSERT INTO wp_posts (post_author, post_date, post_content, post_title) VALUE (1, ?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  // #2 bind params to statement
+  $stmt->bind_param("sss", $date, $body, $title);
+  // #3 execute statement
+  $stmt->execute();
+  // #4 check that row has been inserted $stmt->affected_rows
+  var_dump($stmt);
+  if($stmt->affected_rows == 1) {
+    $id = $stmt->insert_id;
+    // redirect user to the new post they created
+    $location = "Location: article.php?id={$id}&new=true";
+    header($location);
+  }
+
+}
 
  ?>
 <div class="container">
