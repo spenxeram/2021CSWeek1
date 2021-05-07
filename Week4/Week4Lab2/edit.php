5d10212eedf2c17d48e0d47a64b8b24c12d538d2
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-07 14:35:13 +0700

    edit

---
+++ Week4/Week4Lab2/edit.php
@@ -0,0 +1,35 @@
+<?php
+include 'db.php';
+include 'includes/header.php';
+
+if(isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+  $sql = "SELECT * FROM wp_posts WHERE ID = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("s", $id);
+  $stmt->execute();
+  $results = $stmt->get_result();
+  $rows = $results->fetch_assoc();
+  $title = filter_var($rows['post_title'], FILTER_SANITIZE_STRING);
+  $body = filter_var($rows['post_content'], FILTER_SANITIZE_STRING);
+  $id = filter_var($rows['ID'], FILTER_SANITIZE_STRING);
+}
+ ?>
+<div class="container">
+  <h2 class="display-4 mt-3 mb-3">Edit a post....</h2>
+  <div class="row">
+    <form class="" action="edit.php" method="post">
+      <label for="title">Post Title</label>
+      <input type="text" name="title" value="<?php if(isset($title)) {echo $title;} ?>" class="form-control" placeholder="Post title..."value="">
+      <label for="body">Post Content</label>
+      <textarea name="body" class="form-control" rows="8" cols="80"><?php if(isset($body)) {echo $body;} ?></textarea>
+      <button type="submit" class="btn mt-4 btn-block btn-outline-warning" value="<?php if(isset($id)) {echo $id;} ?>" name="submit">Edit Post</button>
+    </form>
+  </div>
+</div>
+
+
+ <?php
+include 'includes/footer.php';
+  ?>
