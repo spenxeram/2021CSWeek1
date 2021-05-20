603e8c20de5d5f507c509a8c194dc7231a7f2bdf
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-20 14:58:56 +0700

    validate

--- Week6/Lect2FileUpload/index.php
+++ Week6/Lect2FileUpload/index.php
@@ -1,5 +1,46 @@
 <?php
 $errors = [];
+if(isset($_POST['submit'])) {
+  $caption = $_POST['caption'];
+  $file = $_FILES['img'];
+  $fname = $file['name'];
+  $ftype = $file['type'];
+  $ftemp = $file['tmp_name'];
+  $ferr = $file['error'];
+  $fsize = $file['size'];
+  $allowed_ext = ['png', 'jpeg', 'jpg'];
+
+  // check to ensure there is no error with the upload
+  if($ferr != 0) {
+    $errors['ferr'] = "File error.";
+  }
+
+  // explore the filetype and check the type and extension
+  $ftype = explode("/", $ftype);
+  if($ftype[0] != "image" && !in_array(end($ftype), $allowed_ext)) {
+    $errors['ftype'] = "Only images can be uploaded.";
+  }
+
+  // check filesize
+  if($fsize > 5000000) {
+    $errors['fsize'] = "File too large.";
+  }
+
+  if(empty($errors)) {
+    $new_filename = uniqid('', true) . "." . end($ftype);
+
+    $new_dest = "images/" . $new_filename;
+    var_dump($new_dest);
+    if(move_uploaded_file($ftemp, $new_dest)) {
+      $errors['success'] = "Your file was moved successfully!";
+    }
+  }
+
+
+
+}
+
+
 
  ?>
 
