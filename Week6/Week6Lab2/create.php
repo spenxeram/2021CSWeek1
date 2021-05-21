c86b195022c30e8a6ddecc66b071631ef42f00db
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-21 13:02:45 +0700

    lab2

---
+++ Week6/Week6Lab2/create.php
@@ -0,0 +1,47 @@
+<?php
+include 'func/config.php';
+include 'func/postmanager.php';
+include 'func/filemanager.php';
+$errors = [];
+if(isset($_POST['submit'])) {
+
+}
+include 'includes/header.php';
+
+ ?>
+<div class="container">
+
+  <div class="row">
+    <?php if ($_SESSION['loggedin'] == false): ?>
+      <div class="mt-5 col-md-6 offset-md-3 text-center">
+        <h2 class="display-5">Please Login to Post!</h2>
+        <p>Create an account or login to post to the website.</p>
+        <button type="button" class="btn btn-block btn-outline-primary"><a href="login.php"><i class="fas fa-sign-in-alt"></i> Create Account/Login</a> </button>
+      </div>
+    <?php else: ?>
+      <div class="mt-3 col-md-6 offset-md-3">
+        <?php if (!empty($errors)): ?>
+          <div class="alert alert-danger" role="alert">
+            <?php var_dump($errors); ?>
+          </div>
+        <?php endif; ?>
+        <h2>Create a post~</h2>
+        <form class="" action="create.php" method="post" enctype="multipart/form-data">
+          <label for="title">Post Title</label>
+          <input type="text" name="title" placeholder="Post title..." value="" class="form-control">
+          <label for="body">Post Content</label>
+          <textarea name="body" class="form-control" rows="8" cols="80"></textarea>
+          <button type="submit" name="submit" class="btn btn-outline-dark btn-block"> <i class="fas fa-edit"></i> Create Post</button>
+        </form>
+      </div>
+      <?php
+
+       ?>
+    <?php endif; ?>
+  </div>
+</div>
+
+
+ <?php
+ include 'includes/footer.php';
+  ?>
---
+++ Week6/Week6Lab2/post.php
@@ -0,0 +1,12 @@
+<?php
+include 'func/config.php';
+include 'func/postmanager.php';
+include 'includes/header.php';
+if(isset($_GET['id'])) {
+  
+}
+ ?>
+
+ <?php
+ include 'includes/footer.php';
+  ?>
