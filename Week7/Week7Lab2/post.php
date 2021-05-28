3b4ac0d6c8b2a01a67086ad0b7beac5805df5078
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-28 13:43:24 +0700

    comment form

--- Week7/Week7Lab2/post.php
+++ Week7/Week7Lab2/post.php
@@ -11,6 +11,7 @@ if(isset($_GET['id'])) {
    <div class="row">
      <?php if ($post == false): ?>
        <h2 class="display-4">404 Post Not Found!</h2>
+      </div>
      <?php else: ?>
        <div class="col-md-8 offset-md-2">
           <img src="<?php echo $post['post_img']; ?>" class="img-fluid" alt="">
@@ -19,9 +20,25 @@ if(isset($_GET['id'])) {
           </em></h5>
            <p><?php echo htmlspecialchars($post['post_body']); ?></p>
        </div>
+     </div> <!-- end of post row -->
 
+     <!-- comment row -->
+      <hr>
+      <h3 class="display-4 mt-3 mb-3">Comments</h3>
+      <hr>
+     <div class="row comments">
+
+       <div class="col-md-8 form">
+         <form class="comment-form" method="POST" action="func/commentmanager.php">
+           <textarea name="comment" class="form-control" rows="4" cols="80"></textarea>
+           <button type="submit" name="comment-submit" class="btn btn-outline-success mt-2"><i class="far fa-comment"></i> Add Comment</button>
+         </form>
+       </div>
+
+
+     </div>
      <?php endif; ?>
-   </div>
+
  </div>
 
  <hr>
