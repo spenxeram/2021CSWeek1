a8468ea7106bcd5de5d711ec17014fed3d46d9d0
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-05-28 14:17:25 +0700

    comment ajax

--- Week7/Week7Lab2/func/commentmanager.php
+++ Week7/Week7Lab2/func/commentmanager.php
@@ -1,3 +1,4 @@
 <?php
-
+ session_start();
+ echo "This comment was sent by ". $_SESSION['user_name']  .": " . $_POST['comment'];
  ?>
--- Week7/Week7Lab2/js/main.js
+++ Week7/Week7Lab2/js/main.js
@@ -1,1 +1,31 @@
 console.log("main js loaded");
+
+// get needed elements
+let theform = document.querySelector("form.comment-form");
+let thecomment = document.querySelector(".comment-form textarea");
+
+// add event listener, prevent default submission and get
+//textarea value
+
+theform.addEventListener("submit", function(event) {
+  event.preventDefault();
+  commentAjax(thecomment.value);
+})
+
+// ajax request
+
+function commentAjax(comment) {
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "func/commentmanager.php", true);
+  // to use the post method we must set the request headers
+  // depending on the form data being sent
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    if(this.status == 200) {
+      console.log(this.responseText);
+    }
+  }
+
+  xhr.send("comment="+comment);
+}
