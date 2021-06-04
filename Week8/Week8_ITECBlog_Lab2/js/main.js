4cfe58c2d90d97c8a3179707f33af00a64bce543
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-06-04 13:12:01 +0700

    Lab2

---
+++ Week8/Week8_ITECBlog_Lab2/js/main.js
@@ -0,0 +1,69 @@
+console.log("main js loaded");
+
+// get needed elements
+let theform = document.querySelector("form.comment-form");
+let thecomment = document.querySelector(".comment-form textarea");
+let hiddeninput = document.querySelector(".comment-form input");
+let commentsdiv = document.querySelector(".comments");
+let commentcard = document.querySelectorAll(".card");
+
+// add event listener, prevent default submission and get
+//textarea value
+
+theform.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  theform.reset();
+})
+
+
+
+// ajax request
+
+function commentAjax(comment, postid, theaction) {
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", theaction, true);
+  // to use the post method we must set the request headers
+  // depending on the form data being sent
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    if(this.status == 200) {
+      console.log(this.responseText);
+
+    }
+  }
+
+  xhr.send("comment="+comment+"&post_id="+postid);
+}
+
+// General function
+function outputNewComment(output) {
+
+  let theoutput = `<div class="col-md-7 mt-2 mb-2"><div class="card"><div class="card-header">${output.user_name} | ${output.date_created}</div>
+  <div class="card-body"><p class="card-text">${output.comment_text}</p>
+  </div></div></div>`;
+
+  theform.insertAdjacentHTML("afterend", theoutput);
+
+}
+
+
+commentcard.forEach((card, i) => {
+  card.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("click");
+    if(e.target.classList.contains("delete-post")){
+      console.log("delete");
+      let par = e.target.parentNode.parentNode.parentNode;
+      console.log(par);
+      par.classList.add("shrinkStart");
+      setTimeout(function(){
+        par.classList.add("shrinkFinish");
+      },100);
+    }
+
+    console.log(e);
+  })
+
+});
---
+++ Week8/Week8_ITECBlog_Lab2/login.php
@@ -0,0 +1,58 @@
+<?php
+include 'config.php';
+include 'func/account.php';
+include 'includes/header.php';
+
+ ?>
+
+<div class="container mt-3">
+   <?php if (isset($errorMsg)): ?>
+     <div class="alert alert-danger" role="alert">
+       <?php echo $errorMsg; ?>
+     </div>
+   <?php endif; ?>
+  <div class="row">
+    <div class="col-md-6">
+      <h3>Create a new account</h3>
+      <hr>
+      <form class="" action="login.php" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" class="form-control" placeholder="Input your username..." value="<?php if (isset($username)) {
+          echo htmlspecialchars($username);}?>">
+
+        <p class="error"><?php if(isset($errors['create_username'])) {echo $errors['create_username'];} ?></p>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control" placeholder="Input your username..." value="<?php if (isset($email)) { echo htmlspecialchars($email);} ?>">
+        <p class="error"><?php if(isset($errors['create_email'])) { echo $errors['create_email'];} ?></p>
+        <label for="password1">Password</label>
+        <input type="password" name="password1" class="form-control" placeholder="Input your username..."value="">
+        <p class="error"></p>
+        <label for="password2">Confirm Password</label>
+        <input type="password" name="password2" class="form-control" placeholder="Input your username..."value="">
+        <p class="error"><?php if(isset($errors['create_password'])) { echo $errors['create_password'];} ?></p>
+        <button type="submit" name="create" class="btn btn-outline-success">Create Account</button>
+      </form>
+    </div>
+    <div class="col-md-6">
+      <h3>Login</h3>
+      <hr>
+      <form class="" action="login.php" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="name" class="form-control" placeholder="Input your username..." value="<?php if (isset($name)) { echo htmlspecialchars($name);} ?>">
+        <p class="error"><?php if(isset($errors['login_username'])) {echo $errors['login_username'];} ?></p>
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Input your username..." value="">
+        <p class="error"><?php if(isset($errors['login_password'])) {echo $errors['login_password'];} ?></p>
+        <button type="submit" name="login" class="btn btn-outline-primary">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php
+$adminpass = "itec2021";
+echo $adminpass . "<br>";
+echo password_hash($adminpass, PASSWORD_DEFAULT);
+include 'includes/footer.php';
+ ?>
