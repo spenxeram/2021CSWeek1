93ac00b801e59a8fb7f33cb35203fbbb9518a115
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-06-18 08:16:00 +0700

    start

--- Week10/Week10Lab1/config.php
+++ Week10/Week10Lab1/config.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-// spl_autoload_register(function ($class) {
-//     $classlocation = 'classes/' . $class . '.php';
-//     if (file_exists($classlocation)) {
-//         require_once $classlocation;
-//     }
-// });
+spl_autoload_register(function ($class) {
+    $classlocation = 'classes/' . $class . '.php';
+    if (file_exists($classlocation)) {
+        require_once $classlocation;
+    }
+});
 if(!isset($_SESSION['loggedin'])) {
   $_SESSION['loggedin'] = false;
 }
@@ -27,11 +27,11 @@ if(!isset($_SESSION['query_history'])) {
   }
 }
 
-Messenger::initializeMessenger();
-$ismsg = Messenger::checkMsg();
+//Messenger::initializeMessenger();
+//$ismsg = Messenger::checkMsg();
 if (isset($ismsg)) {
-  $messages = Messenger::outputMsg();
-  Messenger::clearMsg();
+//  $messages = Messenger::outputMsg();
+//  Messenger::clearMsg();
 }
 
 include 'db.php';
