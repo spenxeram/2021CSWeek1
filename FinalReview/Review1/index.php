66bb5e2ed1a23cc1db3a38fec5842c437c2d169f
spenxeram <55945082+spenxeram@users.noreply.github.com>
2021-06-25 08:28:51 +0700

    front page

--- FinalReview/Review1/index.php
+++ FinalReview/Review1/index.php
@@ -1,40 +1,27 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<?php
+include 'includes/header.php';
+ ?>
 
-  </head>
-  <body>
-    <!-- fixed-top | sticky-top | fixed-bottom -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">ITEC Todo Final Review</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+ <div class="container">
+   <div class="row">
+     <?php if (!$_SESSION['loggedin']): ?>
+       <div class="col-md-6 offset-md-3 mt-5 mb-5">
+         <div class="card" style="width: 18rem;">
+             <div class="card-header">
+                 Please Login!
+             </div>
+             <div class="card-body">
+                 <h5 class="card-title">You must login to add a task!</h5>
+                 <a href="login.php" class="btn btn-primary btn-block"><i class="fa fa-user"></i> Login/Create Account</a>
+             </div>
+         </div>
+       </div>
+     <?php else: ?>
+       to do list here
+     <?php endif; ?>
+   </div>
+ </div>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> <i class="fa fa-user"></i> Login/Create Account</a>
-                    </li>
-            </div>
-        </div>
-    </nav>
-
-
-    <footer style="background:darkgrey; padding 10%; text-align:center">
-      <h1 class="display-4">ITEC TODO 2021</h1>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  </body>
-</html>
+ <?php
+ include 'includes/footer.php';
+  ?>
